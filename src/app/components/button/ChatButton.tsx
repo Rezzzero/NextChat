5f15f13a43003645a1ad0ab13783a4165e6b33a0
Pasha Rezero <kivachuk.2018@gmail.com>
2024-10-16 16:51:12 +0300

    add margin to button

--- src/app/components/button/ChatButton.tsx
+++ src/app/components/button/ChatButton.tsx
@@ -10,7 +10,7 @@ const ChatButton = ({
   return (
     <button
       type="button"
-      className={`max-w-[300px] mx-auto ${
+      className={`max-w-[300px] mt-4 mx-auto ${
         active
           ? "text-[#4fe07f] border-[#4fe07f] hover:bg-[#4fe07f] hover:text-[#1c1c1c]"
           : "text-slate-500 border-slate-500 hover:bg-slate-500 hover:text-[#1c1c1c]"
