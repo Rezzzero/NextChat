7efcc482f207dd12d3dc3196a9e7f21da100be8a
Pasha Rezero <kivachuk.2018@gmail.com>
2024-10-25 15:01:50 +0300

    update navbar

--- src/app/components/Navigation/Navbar.tsx
+++ src/app/components/Navigation/Navbar.tsx
@@ -1,13 +1,27 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const Navbar = () => {
+  const pathName = usePathname();
   return (
     <div className="w-full h-16 bg-slate-500">
       <div className="container max-w-5xl mx-auto flex text-white justify-between pt-4">
         <h1 className="text-2xl font-bold">NextChat.com</h1>
         <div className="flex gap-4 text-lg">
-          <Link href="/chat">Чат</Link>
-          <Link href="/voice-chat">Голосовой чат</Link>
+          <Link
+            href="/chat"
+            className={pathName === "/chat" ? "underline" : ""}
+          >
+            Чат
+          </Link>
+          <Link
+            href="/voice-chat"
+            className={pathName === "/voice-chat" ? "underline" : ""}
+          >
+            Голосовой чат
+          </Link>
         </div>
       </div>
     </div>
